1af8b123a956757ac8d84d5a71c12a42be473bf1
akshith krishna <akshithkrishna47@gmail.com>
2020-11-21 22:45:37 +0530

    day 9

---
+++ day-9/src/com/collectionss/CompartorClass.java
@@ -0,0 +1,19 @@
+package com.collectionss;
+
+import java.util.Comparator;
+
+public class CompartorClass implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		Customer c1 = (Customer) o1;
+		Customer c2 = (Customer) o2;
+		if (c1.accno < c2.accno) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+}
---
+++ day-9/src/com/src/Task2.java
@@ -0,0 +1,19 @@
+package com.src;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap hs=new HashMap();
+		hs.put("a1234", "SteveJobs");
+		hs.put("a1235", "Scott McNealy");
+		hs.put("a1236", "Jeff Bezos");
+		hs.put("a1237", "Larry Ellison");
+		hs.put("a1238", "Bill Gates");
+		System.out.println(hs);
+	}
+
+}
